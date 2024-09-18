db967380f15ccfe76c540f790ee6888e6ab99fd9
Jake Williamson <williamsonj0403@gmail.com>
2024-09-17 20:39:41 -0400

    Declared char variables, printed them to screen.

--- Chapter 2 Checkpoint Exercises (1).cpp
+++ Chapter 2 Checkpoint Exercises (1).cpp
@@ -1,21 +1,15 @@
-// Chapter 2 Checkpoint Exercises (1).cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Terrence Williamson
+//09.17.24
+//Chapter 2 Checkpoint Exercises (1)
+//Checkpoint_2.14
 
 #include <iostream>
 using namespace std;
+char first = 'T', middle = 'J', last = 'W';
 
 int main()
 {
-    cout << "Hello World!\n";
+    cout << first;
+    cout << middle;
+    cout << last;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
