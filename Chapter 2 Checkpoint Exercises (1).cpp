86b5121f05647d0e6b25bbcc779654ba0cb48b5f
Jake Williamson <williamsonj0403@gmail.com>
2024-09-17 23:19:05 -0400

    declared string objects, printed them on the screen with string lines to form sentences appearing on new lines each time.

--- Chapter 2 Checkpoint Exercises (1).cpp
+++ Chapter 2 Checkpoint Exercises (1).cpp
@@ -1,21 +1,19 @@
-// Chapter 2 Checkpoint Exercises (1).cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Terrence Williamson
+//09.17.24
+//Chapter 2 Checkpoint Exercises (1)
+//Checkpoint_2-17
 
 #include <iostream>
 using namespace std;
+string name = "Jake Williamson", address = "8 Fort Worth St", phone_number = "(757)870-0868";
 
 int main()
 {
-    cout << "Hello World!\n";
+    cout << "My name is ";
+    cout << name;
+    cout << "\nI live at ";
+    cout << address;
+    cout << "\nMy phone number is ";
+    cout << phone_number;
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
